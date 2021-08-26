f53085ce39b97720ae3a415d0ef5c4e1b14f242d
CarlosEFPaiva <carlosedufpaiva@poli.ufrj.br>
2021-08-26 10:22:39 -0300

    implemented: each item can now be properly selected. The quantity selector is also implemented, including the item deselection once quantity gets to zero

--- src/Item.js
+++ src/Item.js
@@ -1,17 +1,45 @@
+import React,{useState} from "react";
+
 export default function Item(props) {
+    const {image,alt,name,description,price} = props.data;
+    const [quantity,setQuantity] = useState(0);
+    const [isSelected,setIsSelected] = useState("");
+
+    function decrementQuantity() {
+        if (quantity === 1) {
+            setIsSelected("");
+        }
+        setQuantity(quantity - 1);
+    }
+    
+    function selectItem() {
+        setIsSelected("activated-item");
+        setQuantity(1);
+    }
+
     return (
-        <li onclick="ActivateMainDish(this);">
-        <div>
-            <img src={props.data.image} alt={props.data.alt} />
-            <p className="item-title">
-                {props.data.name}
+        <li className={isSelected} onClick={selectItem}>
+            <div>
+                <img src={image} alt={alt} />
+                <p className="item-title">
+                    {name}
+                </p>
+            </div>
+            <p className="item-subtitle">
+                {description}
             </p>
-        </div>
-        <p className="item-subtitle">
-            {props.data.description}
-        </p>
-        <p className="item-price">{props.data.price}</p>
-        <ion-icon name="checkmark-circle" />
-    </li>
+            <p className="item-price">
+                {price}
+            </p>
+            <div className={`select-quantity ${isSelected}`} onClick = {(e) => e.stopPropagation()} >
+                <button className="red" onClick={decrementQuantity}>
+                    -
+                </button>
+                    {quantity}
+                <button className="green" onClick={() => setQuantity(quantity + 1)}>
+                    +
+                </button>
+            </div>
+        </li>
     );
 }
